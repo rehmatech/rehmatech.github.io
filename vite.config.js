6bb7a88dcf6329ceef996b0824b66381b5fd2c2f
Rehmat Technologies <rehmatech@gmail.com>
2026-03-04 22:50:13 +0530

    Adding Correct Sitename

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rehmatech.github.io/', // must match your GitHub repo name
+  base: '/',
 })
